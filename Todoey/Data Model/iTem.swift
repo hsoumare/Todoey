12c916757a96dc95e42a781d2a69be384850dfe3
Hamidou Soumare <doumacbook@dous-macbook-pro.local>
2018-09-21 11:49:53 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/iTem.swift
@@ -0,0 +1,14 @@
+//
+//  iTem.swift
+//  Todoey
+//
+//  Created by Dou on 9/21/18.
+//  Copyright © 2018 Dou. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
